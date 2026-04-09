87794404cc3004752083ef74823a944763c8bd48
thinhbo1214 <thinhbo1214@gmail.com>
2026-04-09 23:09:02 +0700

    newfile

--- MedicalModule/obj/Release/net8.0-windows/MedicalApp.AssemblyInfo.cs
+++ MedicalModule/obj/Release/net8.0-windows/MedicalApp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicalApp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e3a169b27166d3a7718a1efbaf53717cf67b544")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicalApp")]
